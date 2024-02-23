6c709c41bb8812bf55fc89390881bd9359f26100
Muttahir Mumtaz <muttahir.mumtaz@hotmail.com>
2024-02-23 20:11:58 +0100

    semi structure complete

---
+++ features/general_weather/lib/presentation/ui/general_weather_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GeneralWeatherScreen extends StatefulWidget {
+  const GeneralWeatherScreen({super.key});
+
+  @override
+  State<GeneralWeatherScreen> createState() => _GeneralWeatherScreenState();
+}
+
+class _GeneralWeatherScreenState extends State<GeneralWeatherScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
