5f7dc63ada6268823ee23d6b06cf0103e1a17af9
Muttahir Mumtaz <muttahir.mumtaz@hotmail.com>
2024-02-23 20:29:42 +0100

    upgrades the cocoapods

--- features/general_weather/lib/presentation/ui/general_weather_screen.dart
+++ features/general_weather/lib/presentation/ui/general_weather_screen.dart
@@ -10,6 +10,10 @@ class GeneralWeatherScreen extends StatefulWidget {
 class _GeneralWeatherScreenState extends State<GeneralWeatherScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text('start'),
+      ),
+    );
   }
 }
