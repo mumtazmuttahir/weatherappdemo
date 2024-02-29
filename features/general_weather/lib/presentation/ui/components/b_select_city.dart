b3ae5ec258fee167de9826819e1bf63a53ddbb73
Muttahir Mumtaz <muttahir.mumtaz@hotmail.com>
2024-02-29 13:22:15 +0100

    removes unwanted comments

--- features/general_weather/lib/presentation/ui/components/b_select_city.dart
+++ features/general_weather/lib/presentation/ui/components/b_select_city.dart
@@ -23,7 +23,7 @@ class SelectCitySection extends StatelessWidget {
               color: Colors.black.withOpacity(0.1),
               spreadRadius: 5,
               blurRadius: 7,
-              offset: const Offset(0, 1), // changes position of shadow
+              offset: const Offset(0, 1),
             ),
           ],
         ),
